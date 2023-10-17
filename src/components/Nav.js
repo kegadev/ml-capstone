a59d2763b3af25260d4b91e5504af2e3914a0efb
kegadev <kegadev@outlook.com>
2023-10-17 10:04:02 -0500

    update Nav elements

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,6 +11,15 @@ const Nav = () => {
         <li>
           <a href="#">Menu</a>
         </li>
+        <li>
+          <a href="#">Reservations</a>
+        </li>
+        <li>
+          <a href="#">Order Online</a>
+        </li>
+        <li>
+          <a href="#">Login</a>
+        </li>
       </ul>
     </nav>
   );
