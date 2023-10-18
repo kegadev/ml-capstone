a0680b778c4063a09e46af37ce9e8b5c0e7accee
kegadev <kegadev@outlook.com>
2023-10-18 15:46:41 -0500

    Add default Card to Specials

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -1,5 +1,19 @@
+import Card from "./Card";
+
 const Specials = () => {
-  return <>Specials</>;
+  return (
+    <>
+      <div className="subtitle">
+        <h2>Specials </h2>
+        <button>Online Menu</button>
+      </div>
+      <div className="card-container">
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+      </div>
+    </>
+  );
 };
 
 export default Specials;
