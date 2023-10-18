70a0ddcf152a9deac9d4ea878c5c806589bd7e3e
kegadev <kegadev@outlook.com>
2023-10-18 14:53:01 -0500

    Create Intro.js

---
+++ src/components/Intro.js
@@ -0,0 +1,19 @@
+const Intro = () => {
+  return (
+    <>
+      <div>
+        <h1> Little Lemon</h1>
+        <h2> Chicago</h2>
+        <p>
+          Anim deserunt labore duis commodo velit officia nisi. Amet ex tempor
+          Lorem aliqua dolore tempor excepteur sit adipisicing ullamco ipsum. In
+          culpa incididunt magna proident et pariatur consectetur magna. Eu
+          pariatur reprehenderit incididunt consequat irure veniam anim velit.
+        </p>
+      </div>
+      <div>Image</div>
+    </>
+  );
+};
+
+export default Intro;
