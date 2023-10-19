56434dcdf5280a0074d1f9c20fa481ffd46973ef
kegadev <kegadev@outlook.com>
2023-10-19 13:08:10 -0500

    add menu to mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,24 @@
 import Nav from "./Nav";
 import logo from "../assets/lemon-logo.svg";
 
+import burger from "../assets/burger.svg";
+import { useState } from "react";
 const Header = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  const toggleNav = () => {
+    setShowNav(!showNav);
+  };
+
   return (
     <header>
       <img src={logo} alt="Lemon Logo" />
 
-      <Nav></Nav>
+      <div className="burger" onClick={toggleNav}>
+        <img src={burger} alt="menu icon" />
+      </div>
+
+      {showNav || window.innerWidth > 768.0 ? <Nav /> : null}
     </header>
   );
 };
