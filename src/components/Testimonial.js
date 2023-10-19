02143776d598b3eb9c448c9f889399b7e9cd293e
kegadev <kegadev@outlook.com>
2023-10-19 10:21:29 -0500

    update class name for 5 stars img

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -3,7 +3,7 @@ import stars from "../assets/5stars.png";
 const Testimonial = ({ name, image, description }) => {
   return (
     <div className="testimonial">
-      <img src={stars} alt="5 stars" />
+      <img className="testimonial-stars" src={stars} alt="5 stars" />
       <img className="testimonial-photo" src={image} alt={`${name}  image`} />
       <h3>{name}</h3>
       <p>"{description}"</p>
