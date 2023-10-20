42e8503dce8bbe926542ec2028245c1a2412114c
kegadev <kegadev@outlook.com>
2023-10-20 11:16:30 -0500

    Create ConfirmedBooking.js

---
+++ src/components/ConfirmedBooking.js
@@ -0,0 +1,10 @@
+const ConfirmedBooking = () => {
+  return (
+    <>
+      <h1>Confirmed Booking</h1>
+      <p>Thank you for your reservation!</p>
+    </>
+  );
+};
+
+export default ConfirmedBooking;
