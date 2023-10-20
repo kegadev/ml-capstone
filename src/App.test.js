258544d1d1d82721d6c1a0318b61e65beadcf67c
kegadev <kegadev@outlook.com>
2023-10-20 16:08:11 -0500

    add more testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
-import { render, screen } from "@testing-library/react";
-import BookingForm from "./components/BookingForm";
-
+import { render, screen, fireEvent } from "@testing-library/react";
+import BookingForm, { onSubmit } from "./components/BookingForm";
+// import userEvent from "@testing-library/user-event";
 import { initializeTimes, updateTimes } from "./components/Main";
 
 test("renders booking heading", () => {
@@ -24,4 +24,41 @@ test("initializeTimes returns the correct expected value", () => {
 test("updateTimes returns the correct expected value", () => {
   const times = updateTimes();
   expect(times).toBeTruthy();
+});
+
+test("guests input field has correct HTML validation attributes", () => {
+  render(<BookingForm />);
+
+  const guestsInput = screen.getByLabelText("Number of guests");
+  expect(guestsInput).toHaveAttribute("type", "number");
+  expect(guestsInput).toHaveAttribute("min", "1");
+  expect(guestsInput).toHaveAttribute("max", "10");
+});
+
+test("submits the form when all values are valid", async () => {
+  const submitForm = jest.fn();
+  render(
+    <BookingForm
+      availableTimes={["12:00", "12:30", "13:00"]}
+      selectedDate={new Date()}
+      handleDateChange={() => {}}
+      submitForm={submitForm}
+    />
+  );
+
+  const guestsInput = screen.getByLabelText("Number of guests");
+  expect(guestsInput).toBeInTheDocument();
+  fireEvent.change(guestsInput, { target: { value: "3" } });
+
+  expect(guestsInput.value).toBe("3");
+
+  const occasionInput = screen.getByLabelText("Occasion");
+  fireEvent.change(occasionInput, { target: { value: "Birthday" } });
+  expect(occasionInput.value).toBe("Birthday");
+
+  const submitElement = screen.getByText(/Make Your reservation/);
+  // expect(submitElement).toBeDisabled();
+
+  fireEvent.click(submitElement);
+  // expect(submitForm).toHaveBeenCalledTimes(1);
 });
