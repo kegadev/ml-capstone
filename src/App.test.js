9a4b440b7d6280a99511be7c44b570a030c5581b
kegadev <kegadev@outlook.com>
2023-10-20 08:42:21 -0500

    chore: remove console.logs

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,7 @@ import { render, screen } from "@testing-library/react";
 import BookingForm from "./components/BookingForm";
 
 test("renders booking heading", () => {
-  console.log("correr test");
   render(<BookingForm />);
-  console.log("Ya se renderizo");
 
   const linkElement = screen.getByText(/Choose date/);
   expect(linkElement).toBeInTheDocument();
