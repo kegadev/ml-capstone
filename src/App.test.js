b27f32ba2c2bba0fdb163600c6bc138f8d2b0582
kegadev <kegadev@outlook.com>
2023-10-20 09:23:37 -0500

    add test for initialize times

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import { render, screen } from "@testing-library/react";
 import BookingForm from "./components/BookingForm";
 
+import { initializeTimes } from "./components/Main";
+
 test("renders booking heading", () => {
   render(<BookingForm />);
 
   const linkElement = screen.getByText(/Choose date/);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("initializeTimes returns the correct expected value", () => {
+  const initialState = {
+    availableTimes: [],
+    selectedTime: "",
+  };
+
+  const times = initializeTimes();
+  expect(times).toHaveLength(7);
 });
--- src/components/Main.js
+++ src/components/Main.js
@@ -61,4 +61,5 @@ const Main = () => {
   );
 };
 
+export { initializeTimes, updateTimes };
 export default Main;
